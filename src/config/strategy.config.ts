fa99059eb5925adaa23f200370d3623135cc58ba
zeng.516 <zeng.516@bytedance.com>
2026-04-21 11:58:27 +0800

    first commit

--- src/config/strategy.config.ts
+++ src/config/strategy.config.ts
@@ -11,7 +11,7 @@ const Config = {
     prod: {
       LONGPORT_APP_KEY: "e8c06d148da98f63f04c4eae10db96ac",
       LONGPORT_APP_SECRET: "d8118038165ce555672118f09bd81382629f279d142e5d05023150199fb23213",
-      LONGPORT_ACCESS_TOKEN: "m_eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJsb25nYnJpZGdlIiwic3ViIjoiYWNjZXNzX3Rva2VuIiwiZXhwIjoxNzcyNTQ3NTkyLCJpYXQiOjE3NjQ3NzE1OTMsImFrIjoiZThjMDZkMTQ4ZGE5OGY2M2YwNGM0ZWFlMTBkYjk2YWMiLCJhYWlkIjoyMTAwMzQ4NiwiYWMiOiJsYl9wYXBlcnRyYWRpbmciLCJtaWQiOjE4OTMxMDA0LCJzaWQiOiJ0Z2o1RDNsOElvVHRpR1JGdTdTRnpnPT0iLCJibCI6MywidWwiOjAsImlrIjoibGJfcGFwZXJ0cmFkaW5nXzIxMDAzNDg2In0.EF7s5u-Z5O_jVjlmu9PwwvhYyVfzuiW5S2jM3FxLf81M4ZQAm7i1McQ2Hmhhnk_uw7fjsxxlrKfNo2tunsK0I3LLYRLFbYX7F6JcuqJnxXFebfpGrSyUmGWsrgP5OofZWMm7Sl9_H0Hab1xojgjYdOsSUceg7DsBkSKF1yzYGYjB3NEJ4zdukMoeY_ncjiVBxy7RUm6-XDNY6cobl3_E3Z3vkjcCgH8WphmHHw_e-oTY9G0NkHzv0PYb_hUHDf9oykgsx-AGqamhCy4Mbi0YB7dN0NsHonGc3mE2qk1YYNBi93gJtp2ke67xnb5hjj8RZ7VTAHSHJlnJUtcTaLJ8hcGmZGYk0x8wc3ftgQkJbHpTeikHT2-pH4RsMWPWSlIwnEtCxPrZxFSOey3uTrtnH8sW1v0au-3nttbA259AM5qkkjymKQqIX0hF4bKeR3xnpPC_Px40hyqPRwXh-6KJQZdX8ibDfo8IHujIh4q0JvxS5easEalRg6UvMY2u0OoUjM6UPauGRlGIiVoLTSxrlkxI0KtJMqhu8GIO4BuZdJyqWBp2eiQ1REzUGeI9zCS5ebksU-uqTqfiFgjKBv5G_ddAMiHnnulcgO_6qj4soVhOeucr4Ho_6t0_Zl-4xUwB7EF1Ad2nUVGv6n5_sqNgcytcSpRepKNZOFKoK9IbxAw"
+      LONGPORT_ACCESS_TOKEN: "m_eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ5YWRiMGIxYTdlNzYxNzEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJsb25nYnJpZGdlIiwic3ViIjoiYWNjZXNzX3Rva2VuIiwiZXhwIjoxNzgwNTgzNzYyLCJpYXQiOjE3NzI4MDc3NjMsImFrIjoiZThjMDZkMTQ4ZGE5OGY2M2YwNGM0ZWFlMTBkYjk2YWMiLCJhYWlkIjoyMTAwMzQ4NiwiYWMiOiJsYl9wYXBlcnRyYWRpbmciLCJtaWQiOjE4OTMxMDA0LCJzaWQiOiJnNDVZZitOS0Zoak40bUFDSE5Id0h3PT0iLCJibCI6MywidWwiOjAsImlrIjoibGJfcGFwZXJ0cmFkaW5nXzIxMDAzNDg2In0.cErLlWN-l8K7YK1D-WrJPW7DMWhbtB3MyoA5JFVjKDe_v3YT8uo8702fh-s9DiUpZjrTRDUWfQqpBt9srZSFBrsi0B6biKWEttdEgLQgp2DZ0talg5PmWJ-fkWZ5jj0ydC_7ei2nUCcA7asZKBen3yTTsAldJt02KLg7QLRHxxOGJCJUVX2YkgTOrusTRwWNBBaD_lxpzbnlimBidBg9FLbr7IvVzXoXA7Y3KyKUu46Oo0pAJy5Ro2A23T-N4S1wldItVD_LjHKxeTJ8C4f7b-9-Uco0emoOV7hWLJKlz45feZeIdKAHKHq_3Ou_KBRTk6JKDSdKX7mioIVmLtGvf4997TknU95hprBQMt1ceqFOTL-ebMiIZqa4teTDi-OpsHHUdbJLi6W7J1MVvFP9AHq2oz3z2v7HGm41JPiReyEHM4i6PCRjX8g07mKxpCdoUABk8bPRZ4K9JSi3Cpni3yocTkGuGcwttsRHsbWRRKdSRTljvw1Z1YdrLdiSkCsiaU8_aG04y3HDzPAVxPLtlqN3cWKoLMQ83TeqDQJpnSjuboOQdNICRu-QHHSk_JlHBbc5AjyenEtPo4cBv5qV5oZKDF6UeEw-o9bj5XePb2qz2IPP_2aDsrZWDeM1qS-MVN68OD8iMB4wG44vaHSvNenHpg654ZlfNCXjmLlBFPg"
     },
   },
   // ========================
@@ -27,7 +27,7 @@ const Config = {
     'SMCI', 'CRDO', 'MU', 'PLTR', 'NFLX', 'LLY', 'LULU', 'CIEN', 'TME', 'NOK', 'NET',
     'SATS', 'LITE', 'WDC', 'RIVN', 'NOW', 'COHR', 'FCX', 'STX', 'VRT', 'JD', 'BX', 'GLW',
     'TEM', 'RVMD', 'UNH', 'CVX', 'VG', 'COST', 'RDDT', 'SE', 'NKE', 'PBR', 'PFE', 'CRCL',
-    'ALAB', 'ARM', 'TSEM', 'AMAT','SOFI', 'NBIS', 'CRWV', 'SNDK', 'ASML', 'ALB', 'CRM',
+    'ALAB', 'ARM', 'TSEM', 'AMAT', 'SOFI', 'NBIS', 'CRWV', 'SNDK', 'ASML', 'ALB', 'CRM',
     'VST', 'IESC', 'CAR', 'ONTO', 'GFS', 'RYAAY', 'LYB', 'DOW', 'CF'
   ].map(s => s + '.US'),
 
@@ -71,7 +71,7 @@ const Config = {
   // ========================
   // 成交量 区间参数
   // ========================
-  volumePeriod: 15, 
+  volumePeriod: 15,
   volumeEntryThreshold: 1.2, // 1.2倍当前成交量
   breakVolumePeriod: 5, // 突破区间
   postVolumePeriod: 10, // 和过去10分钟成交量对比
